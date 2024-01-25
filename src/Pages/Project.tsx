be19ad9414bbcd9a8a161a1f41b18076a36646ed
ViNu-23 <vijayvinu46@gmail.com>
2024-01-25 15:35:50 +0530

    data updated from project

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -22,16 +22,25 @@ export default function Project() {
 
             <div className='project-container' style={{width:'100%'}}>
                 <div className='project-info'>
-                    <div className='project-no'>Project 01</div>
-                    <div className='project-name'>CXOSpace</div>
-                    <div className='project-dec'>B2B Saas Product</div>
+                    <div className='project-no'>Project 02</div>
+                    <div className='project-name'>Salesken</div>
+                    <div className='project-dec'>Conversational Intelligence Platform</div>
 
                     <p>
-                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
+                    Worked on the platform, which helps sales team to improve their
+performance by getting real-time cues during their conversation with
+the customer.
+Designed modules for Agent, Manager & Admin.
+
                     </p>
                     <p>
                         <div className='role'>My Role:</div> 
-                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
+                        <div>Led the design process and helped the Founder to accomplish his
+expectation and managed three designers.
+Created Wireframes, Prototype, Design System and High-fidelity
+Designs for Web and Mobile App.
+Collaborated with tech team and aligned the design guidelines.
+Created Brand Identity and Marketing Collateral. </div>
                     </p>
                 </div>
                 <div><img src="./src/Images/Picture4.png" alt='project' className='project-img'/></div>
@@ -40,16 +49,19 @@ export default function Project() {
 
             <div className='project-container' style={{width:'100%'}}>
                 <div className='project-info'>
-                    <div className='project-no'>Project 01</div>
-                    <div className='project-name'>CXOSpace</div>
-                    <div className='project-dec'>B2B Saas Product</div>
+                    <div className='project-no'>Project 03</div>
+                    <div className='project-name'>Talentify</div>
+                    <div className='project-dec'>Educational Training Platform</div>
 
                     <p>
-                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
+                    Talentify is a digital training platform, designed beautifully with a
+gamification concept to improve users professional careers.
                     </p>
                     <p>
                         <div className='role'>My Role:</div> 
-                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
+                        <div>Redesigned existing products and delivered better visual solutions.
+also helped to make better User Interactions, created different types
+of gamification concepts for each lesson.</div>
                     </p>
                 </div>
                 <div><img src="./src/Images/Picture5.png" alt='project' className='project-img'/></div>
@@ -58,16 +70,24 @@ export default function Project() {
 
             <div className='project-container' style={{width:'100%'}}>
                 <div className='project-info'>
-                    <div className='project-no'>Project 01</div>
-                    <div className='project-name'>CXOSpace</div>
-                    <div className='project-dec'>B2B Saas Product</div>
+                    <div className='project-no'>Project 04</div>
+                    <div className='project-name'>My Joining Bonus</div>
+                    <div className='project-dec'>Job Portal</div>
 
                     <p>
-                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
+                    MJB is a portal to capture and serve the needs of recruiters and
+Job Seekers. also the system map and filter the candidates who are
+potentially best suited for the job requirements.
+Designed modules for Recruiters and Job Seekers.
                     </p>
                     <p>
                         <div className='role'>My Role:</div> 
-                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
+                        <div>Responsible for Planning/Research, Wireframe, Information
+Architecture, Interaction, Design System and High-fidelity Visual
+Designs.
+Worked with CTO & Dev Team to ensure development is aligned as
+per design.
+Created Brand Identity and Marketing Collateral.</div>
                     </p>
                 </div>
                 <div><img src="./src/Images/Picture6.png" alt='project' className='project-img'/></div>
@@ -76,16 +96,17 @@ export default function Project() {
 
             <div className='project-container' style={{width:'100%'}}>
                 <div className='project-info'>
-                    <div className='project-no'>Project 01</div>
-                    <div className='project-name'>CXOSpace</div>
-                    <div className='project-dec'>B2B Saas Product</div>
+                    <div className='project-no'>Project 05</div>
+                    <div className='project-name'>  </div>
+                    <div className='project-dec'>Application Tracking System</div>
 
                     <p>
-                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
+                    Built the product to complete recruitment solution to accelerate hiring process, it manages jobs, candidates, interviews.
                     </p>
                     <p>
                         <div className='role'>My Role:</div> 
-                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
+                        <div>Created Interaction, Design System and
+High Fidelity Visual Designs.</div>
                     </p>
                 </div>
                 <div><img src="./src/Images/Picture7.png" alt='project' className='project-img'/></div>
@@ -94,16 +115,18 @@ export default function Project() {
 
             <div className='project-container' style={{width:'100%'}}>
                 <div className='project-info'>
-                    <div className='project-no'>Project 01</div>
-                    <div className='project-name'>CXOSpace</div>
-                    <div className='project-dec'>B2B Saas Product</div>
+                    <div className='project-no'>Project 06</div>
+                    <div className='project-name'>Firaa</div>
+                    <div className='project-dec'>Travel Blogging Platform</div>
 
                     <p>
-                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
+                    Firaa is a Travel Blogging app where the traveller can share their
+experiences, offline too and create a memorable trip with pictures
+and videos.
                     </p>
                     <p>
                         <div className='role'>My Role:</div> 
-                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
+                        <div>Created Visual Design for Website and Android Application.</div>
                     </p>
                 </div>
                 <div><img src="../src/Images/Picture8.png" alt='project' className='project-img'/></div>
