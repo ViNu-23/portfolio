205c2f3c6a7ce1e3df2ad883e310007f32203e45
ViNu-23 <vijayvinu46@gmail.com>
2024-01-25 15:14:25 +0530

    fix

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -17,7 +17,7 @@ export default function Project() {
                         <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
                     </p>
                 </div>
-                <div><img src="./Images/Picture3.png" alt='project' className='project-img'/></div>
+                <div><img src="./src/Images/Picture3.png" alt='project' className='project-img'/></div>
             </div>
 
             <div className='project-container' style={{width:'100%'}}>
@@ -34,25 +34,7 @@ export default function Project() {
                         <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
                     </p>
                 </div>
-                <div><img src="./Images/Picture4.png" alt='project' className='project-img'/></div>
-            </div>
-
-
-            <div className='project-container' style={{width:'100%'}}>
-                <div className='project-info'>
-                    <div className='project-no'>Project 01</div>
-                    <div className='project-name'>CXOSpace</div>
-                    <div className='project-dec'>B2B Saas Product</div>
-
-                    <p>
-                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
-                    </p>
-                    <p>
-                        <div className='role'>My Role:</div> 
-                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
-                    </p>
-                </div>
-                <div><img src="./Images/Picture5.png" alt='project' className='project-img'/></div>
+                <div><img src="./src/Images/Picture4.png" alt='project' className='project-img'/></div>
             </div>
 
 
@@ -70,7 +52,7 @@ export default function Project() {
                         <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
                     </p>
                 </div>
-                <div><img src="./Images/Picture6.png" alt='project' className='project-img'/></div>
+                <div><img src="./src/Images/Picture5.png" alt='project' className='project-img'/></div>
             </div>
 
 
@@ -88,7 +70,7 @@ export default function Project() {
                         <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
                     </p>
                 </div>
-                <div><img src="./Images/Picture7.png" alt='project' className='project-img'/></div>
+                <div><img src="./src/Images/Picture6.png" alt='project' className='project-img'/></div>
             </div>
 
 
@@ -106,7 +88,25 @@ export default function Project() {
                         <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
                     </p>
                 </div>
-                <div><img src="./Images/Picture8.png" alt='project' className='project-img'/></div>
+                <div><img src="./src/Images/Picture7.png" alt='project' className='project-img'/></div>
+            </div>
+
+
+            <div className='project-container' style={{width:'100%'}}>
+                <div className='project-info'>
+                    <div className='project-no'>Project 01</div>
+                    <div className='project-name'>CXOSpace</div>
+                    <div className='project-dec'>B2B Saas Product</div>
+
+                    <p>
+                        Built a product for the CXO level community to exchange their knowledge, to hassle-free clarify their doubts and buy or know about required tools. The main key factors of this platform are Learn, Engage, Connect, and Buy.
+                    </p>
+                    <p>
+                        <div className='role'>My Role:</div> 
+                        <div>Led the entire design process, worked with Founder and Product Head, was partially involved in user research. Created Wireframes, Prototype, Design System, and High-fidelity Designs for Web, Responsive, and Mobile App. Provided PRD's to development team, helped testing team to find bugs.</div>
+                    </p>
+                </div>
+                <div><img src="./src/Images/Picture8.PNG" alt='project' className='project-img'/></div>
             </div>
         </>
     );
