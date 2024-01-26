3318fa06598bf46a1f94b3cd2034c8de90f97f72
ViNu-23 <vijayvinu46@gmail.com>
2024-01-26 12:51:31 +0530

    optimized images

--- src/Pages/CaseStudies.tsx
+++ src/Pages/CaseStudies.tsx
@@ -1,21 +1,46 @@
-import Menu from "./Menu"
+import React from 'react';
+import Menu from './Menu';
 
 export default function CaseStudies() {
   const caseStudy = {
-    width: '100%'
-  }
+    width: '100%',
+  };
+
+  const projectPaths = [
+    "/Images/Picture9.jpg",
+    "/Images/Picture10.png",
+    "/Images/Picture11.jpg",
+    "/Images/Picture12.png",
+    "/Images/Picture13.png",
+    "/Images/Picture14.png",
+    "/Images/Picture15.png",
+    "/Images/Picture16.png",
+    "/Images/Picture17.png",
+    "/Images/Picture18.png",
+    "/Images/Picture19.png",
+    "/Images/Picture20.png",
+    "/Images/Picture21.png",
+    "/Images/Picture24.png",
+    "/Images/Picture25.png",
+  ];
+
   return (
     <>
-      <div>
-        <h2 className='title' style={{ margin: '25px 0px 20px 25px' }}>Case Studies</h2>
-        <div><img style={caseStudy} src="../Images/Picture9.jpg" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="../Images/Picture10.png" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="../Images/Picture11.jpg" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="../Images/Picture12.png" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="../Images/Picture13.png" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="../Images/Picture14.png" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="..//Images/Picture15.png" alt='project' className='project-img' /></div>
-        <div><img style={caseStudy} src="../Images/Picture16.png" alt='project' className='project-img' /></div>
+      <div style={{ marginBottom: '50px' }}>
+        <h2 className='title' style={{ margin: '25px 0px 20px 25px' }}>
+          Case Studies
+        </h2>
+        {projectPaths.map((path, index) => (
+          <div key={index}>
+            <img
+              style={caseStudy}
+              src={path}
+              alt={`project-${index + 1}`}
+              className='project-img'
+              loading="lazy"
+            />
+          </div>
+        ))}
       </div>
       <div
         style={{
@@ -27,5 +52,5 @@ export default function CaseStudies() {
         <Menu />
       </div>
     </>
-  )
+  );
 }
